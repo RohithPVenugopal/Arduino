3dbd9521f3be407babfa19452bc4dc0ba716c8a5
Rohith P V <130417311+RohithPVenugopal@users.noreply.github.com>
2026-01-26 11:01:16 +0530

    Create Laser_module_blink.ino

---
+++ Laser_module_blink.ino
@@ -0,0 +1,12 @@
+int laserPin = 13;
+
+void setup() {
+  pinMode(laserPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(laserPin, HIGH); // Laser ON
+  delay(1000);
+  digitalWrite(laserPin, LOW);  // Laser OFF
+  delay(1000);
+}
